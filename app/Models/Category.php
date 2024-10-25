2286b5018ee8efbfed8a7fe494486c432a1ff97d
delmat238 <matdelhomel@gmail.com>
2024-10-25 15:45:56 +0200

    ajout du seeder de Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Http\Request;
 
 class Category extends Model
 {
     use HasFactory;
+
+    public function create(Request $request): \Illuminate\Http\JsonResponse
+    {
+
+    }
 
     public function tracks(): HasMany
     {
